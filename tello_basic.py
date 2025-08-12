cfa63d16a17e2b4789aa4c003147e0d8e59f59f0
Katellyn Hyker <87871440+mxneo00@users.noreply.github.com>
2025-08-12 11:19:35 -0400

    Add files via upload

---
+++ tello_basic.py
@@ -0,0 +1,32 @@
+from djitellopy import Tello
+import time
+
+def main():
+    # Create Tello object
+    tello = Tello()
+
+    # Connect to the drone
+    tello.connect()
+    print(f"Battery: {tello.get_battery()}%")
+
+    # Take off
+    tello.takeoff()
+
+    # Fly forward 50 cm
+    tello.move_forward(150)
+
+    # Rotate clockwise 90 degrees
+    tello.rotate_clockwise(90)
+
+    # Hover for 2 seconds
+    time.sleep(2)
+
+    tello.rotate_clockwise(90)
+    time.sleep(5)
+
+    # Land
+    tello.land()
+
+if __name__ == "__main__":
+    main()
+
